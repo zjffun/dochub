fd63ab487b63e34f758346b4f8985499c8654c97
Jufeng Zhang <zjffun@gmail.com>
2023-01-14 12:16:54 +0800

    feat: style relation list

--- src/pages/RelationList.tsx
+++ src/pages/RelationList.tsx
@@ -1,8 +1,11 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import ReactPaginate from "react-paginate";
 import { getListGroupByPath } from "../api";
 import Header from "../components/Header";
 import { IRelationGroupByPath } from "../types";
+
+import "./RelationList.scss";
 
 function getHref(
   docName: string | undefined,
@@ -17,9 +20,15 @@ function getHref(
 
 function RelationList() {
   const [list, setList] = useState<IRelationGroupByPath[]>([]);
+  const [pageCount, setPageCount] = useState(10);
+  const [forcePage, setForcePage] = useState(0);
 
   const params = useParams();
   const nameId = params.nameId;
+
+  function handlePageClick(data: any) {
+    setForcePage(data.selected);
+  }
 
   useEffect(() => {
     if (!nameId) return;
@@ -36,24 +45,54 @@ function RelationList() {
   return (
     <>
       <Header></Header>
-      <div className="dochub-relation-list-wrapper">
-        <ul className="dochub-relation-list">
+      <div className="dochub-relation-table-wrapper">
+        <table className="dochub-relation-table">
+          <tr className="dochub-relation-table__header">
+            <th className="dochub-relation-table__th">#</th>
+            <th className="dochub-relation-table__th">Name</th>
+            <th className="dochub-relation-table__th">Consistent</th>
+            <th className="dochub-relation-table__th">Translated</th>
+            <th className="dochub-relation-table__th"></th>
+          </tr>
           {list.map((item) => {
             return (
-              <li className="dochub-relation-list__item" key={item.fromPath}>
-                <a
-                  target="_blank"
-                  rel="noreferrer"
-                  href={getHref(nameId, item.fromPath, item.toPath)}
-                >
-                  <span>{item.fromPath}</span>
-                  <span>&nbsp;---&gt;---&nbsp;</span>
-                  <span>{item.toPath}</span>
-                </a>
-              </li>
+              <tr className="dochub-relation-table__tr" key={item.fromPath}>
+                <td className="dochub-relation-table__td dochub-relation-table__id">
+                  1
+                </td>
+                <td className="dochub-relation-table__td dochub-relation-table__name">
+                  {item.toPath}
+                </td>
+                <td className="dochub-relation-table__td dochub-relation-table__consistent">
+                  1
+                </td>
+                <td className="dochub-relation-table__td dochub-relation-table__translated">
+                  2
+                </td>
+                <td className="dochub-relation-table__td dochub-relation-table__operation">
+                  <a href={getHref(nameId, item.fromPath, item.toPath)}>
+                    translate
+                  </a>
+                </td>
+              </tr>
             );
           })}
-        </ul>
+          <tr className="dochub-relation-table__footer">
+            <td colSpan={5}>
+              <ReactPaginate
+                className="dochub-relation-table__pagination"
+                breakLabel="..."
+                nextLabel="next >"
+                onPageChange={handlePageClick}
+                pageRangeDisplayed={5}
+                forcePage={forcePage}
+                pageCount={pageCount}
+                previousLabel="< previous"
+                renderOnZeroPageCount={() => null}
+              />
+            </td>
+          </tr>
+        </table>
       </div>
     </>
   );
