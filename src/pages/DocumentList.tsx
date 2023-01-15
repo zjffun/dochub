21df238c97210016193a40d9c99431253fd24f3c
Jufeng Zhang <zjffun@gmail.com>
2023-01-15 19:39:40 +0800

    feat: update icon

--- src/pages/DocumentList.tsx
+++ src/pages/DocumentList.tsx
@@ -31,42 +31,43 @@ function DocumentList() {
                   style={{
                     backgroundImage: `url(${item.logoUrl})`,
                   }}
-                ></div>
-                <a className="dochub-doc-list__item__link" href={item.docUrl}>
-                  <div className="dochub-project-card">
-                    <h2 className="dochub-project-card__name">{item.name}</h2>
-                    <p className="dochub-project-card__desc">{item.desc}</p>
-                    <div className="dochub-progress">
-                      <div className="dochub-progress__bar">
-                        <div
-                          className="dochub-progress__bar__translated"
-                          style={{ right: `${100 - translated}%` }}
-                        ></div>
-                        <div
-                          className="dochub-progress__bar__consistent"
-                          style={{ right: `${100 - consistent}%` }}
-                        ></div>
-                      </div>
-                      <div className="dochub-progress__detail">
-                        {`${consistent}% `}
-                        <span className="dochub-progress__detail__consistent">
-                          consistent
-                        </span>
-                        {`${translated}%`}
-                        <span className="dochub-progress__detail__translated">
-                          translated
-                        </span>{" "}
-                        |{" "}
-                        <Link
-                          className="dochub-progress__detail__contribute"
-                          to={`/doc/${item.nameId}`}
-                        >
-                          contribute
-                        </Link>
+                >
+                  <a className="dochub-doc-list__item__link" href={item.docUrl}>
+                    <div className="dochub-project-card">
+                      <h2 className="dochub-project-card__name">{item.name}</h2>
+                      <p className="dochub-project-card__desc">{item.desc}</p>
+                      <div className="dochub-progress">
+                        <div className="dochub-progress__bar">
+                          <div
+                            className="dochub-progress__bar__translated"
+                            style={{ right: `${100 - translated}%` }}
+                          ></div>
+                          <div
+                            className="dochub-progress__bar__consistent"
+                            style={{ right: `${100 - consistent}%` }}
+                          ></div>
+                        </div>
+                        <div className="dochub-progress__detail">
+                          {`${consistent}% `}
+                          <span className="dochub-progress__detail__consistent">
+                            consistent
+                          </span>
+                          {`${translated}%`}
+                          <span className="dochub-progress__detail__translated">
+                            translated
+                          </span>{" "}
+                          |{" "}
+                          <Link
+                            className="dochub-progress__detail__contribute"
+                            to={`/doc/${item.nameId}`}
+                          >
+                            contribute
+                          </Link>
+                        </div>
                       </div>
                     </div>
-                  </div>
-                </a>
+                  </a>
+                </div>
               </li>
             );
           })}
